20cedbd61d31ae87e1d389170fa7412157eb75f0
akbar904 <zharfan@walturn.com>
2024-10-18 08:47:41 +0700

    Update files

--- src/lib/features/onboarding/welcome_view.dart
+++ src/lib/features/onboarding/welcome_view.dart
@@ -1,11 +1,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:my_app/features/onboarding/welcome_viewmodel.dart';
 
 class WelcomeView extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
-		return ViewModelBuilder.reactive(
+		return ViewModelBuilder<WelcomeViewModel>.reactive(
 			viewModelBuilder: () => WelcomeViewModel(),
 			builder: (context, model, child) => Scaffold(
 				body: SafeArea(
@@ -42,11 +43,5 @@ class WelcomeView extends StatelessWidget {
 				),
 			),
 		);
-	}
-}
-
-class WelcomeViewModel extends BaseViewModel {
-	void navigateToAuthOptions() {
-		// Navigation logic to AuthOptionsView
 	}
 }
